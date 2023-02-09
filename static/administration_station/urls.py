0cdcf064ecc4074d2a6ceaee7b0d3ed51d83bfa8
AlfieAnil <72808652+AlfieAnil@users.noreply.github.com>
2023-02-09 22:09:16 +0000

    Delete static/administration_station directory

--- static/administration_station/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.loginPage, name="login"),
-    path('create-user', views.registerPage, name="create-user"),
-    path('create-priest-message', views.createPriestMessage, name="create-priest-message"),
-    path('upload-newsletter', views.NewsletterUpload, name='upload-newsletter')
-]
